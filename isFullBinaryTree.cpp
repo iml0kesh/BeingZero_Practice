99f68eb6a59c9cde427b68345970c999adacba16
lokesh matta <iamlokeshofficial@gmail.com>
2023-01-20 14:57:13 +0530

    java code added

--- isFullBinaryTree.cpp
+++ isFullBinaryTree.cpp
@@ -20,3 +20,32 @@ bool isFullBinaryTree(BSTNode *root)
         return (isFullBinaryTree(root->left) && isFullBinaryTree(root->right));
     return false;
 }
+
+
+//in java
+
+import java.util.*;
+
+class BSTNode{
+    int val;
+    BSTNode left, right;
+    BSTNode(int d){
+      	val = d;
+      	left = right = null;
+    }
+}
+
+class BeingZero
+{
+    boolean isFullBinaryTree(BSTNode root)
+    {
+      // TODO:  Your Code Goes Here
+      if(root == null) return true;
+      if(root.left == null && root.right ==null) return true;
+      if(root.left !=null && root.right !=null){
+          return isFullBinaryTree(root.left) && isFullBinaryTree(root.right);
+      }
+      return false;
+    }
+}
+
